24a7f0f63bb331a2a951cacb313683838b2ab086
spyth <sheshaopeng@msn.com>
2017-10-12 09:30:44 +0800

    remove previous tab when switch

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function(tab){
     createNewTabInOppositeMode(tab.url, tab.incognito);
+    chrome.tabs.remove(tab.id);
 });
 
 chrome.contextMenus.create({
-  id: "showupornot",
+  id: "incognitoornot",
   title: "Open Link in Incognito/Normal Window",
   contexts: ["link"]
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+chrome.contextMenus.onClicked.addListener(function(info, tab){
     createNewTabInOppositeMode(info.linkUrl, tab.incognito);
 });
 
 function createNewTabInOppositeMode(url, incognito){
   chrome.windows.getAll({windowTypes: ["normal"]}, function(windows){
     for(var i = 0; i < windows.length; i++ ){
-      var windowId = 0;
       if(windows[i].incognito != incognito){
         chrome.tabs.create({windowId: windows[i].id, url: url, active: true});
         return;

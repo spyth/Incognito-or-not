9857e5eacb2a3cebda5d1065543cbc44dbb99d8f
spyth <sheshaopeng@gmail.com>
2021-06-15 14:33:29 +0800

    Active window when switch

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.browserAction.onClicked.addListener(function(tab){
-    createNewTabInOppositeMode(tab.url, tab.incognito);
-    chrome.tabs.remove(tab.id);
+chrome.browserAction.onClicked.addListener(function (tab) {
+  createNewTabInOppositeMode(tab.url, tab.incognito);
+  chrome.tabs.remove(tab.id);
 });
 
 chrome.contextMenus.create({
@@ -9,18 +9,20 @@ chrome.contextMenus.create({
   contexts: ["link"]
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab){
-    createNewTabInOppositeMode(info.linkUrl, tab.incognito);
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  createNewTabInOppositeMode(info.linkUrl, tab.incognito);
 });
 
-function createNewTabInOppositeMode(url, incognito){
-  chrome.windows.getAll({windowTypes: ["normal"]}, function(windows){
-    for(var i = 0; i < windows.length; i++ ){
-      if(windows[i].incognito != incognito){
-        chrome.tabs.create({windowId: windows[i].id, url: url, active: true});
+function createNewTabInOppositeMode(url, incognito) {
+  chrome.windows.getAll({ windowTypes: ["normal"] }, function (windows) {
+    for (var i = 0; i < windows.length; i++) {
+      if (windows[i].incognito != incognito) {
+        chrome.windows.update(windows[i].id, { focused: true }, function (focused_window) {
+          chrome.tabs.create({ windowId: focused_window.id, url: url, active: true });
+        });
         return;
       }
     }
-    chrome.windows.create({url: url, incognito: !incognito});
-  })
+    chrome.windows.create({ url: url, incognito: !incognito });
+  });
 }

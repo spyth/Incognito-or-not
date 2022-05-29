fee374594e9211243dee489eaad961b7ca5df8f1
spyth <sheshaopeng@gmail.com>
2022-05-29 17:28:18 +0800

    Migrate manifest v3

--- background.js
+++ background.js
@@ -1,21 +1,59 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener((tab) => {
   createNewTabInOppositeMode(tab.url, tab.incognito);
   chrome.tabs.remove(tab.id);
 });
 
-chrome.contextMenus.create({
-  id: "incognitoornot",
-  title: "Open Link in Incognito/Normal Window",
-  contexts: ["link"]
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "linkItem",
+    title: chrome.i18n.getMessage("linkContextMenu"),
+    contexts: ["link"]
+  });
+  chrome.contextMenus.create({
+    id: "pageItem",
+    title: chrome.i18n.getMessage("pageContextMenu"),
+    contexts: ["page"]
+  });
+  chrome.contextMenus.create({
+    id: "selectionItem",
+    title: chrome.i18n.getMessage("textContextMenu"),
+    contexts: ["selection"]
+  });
 });
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  createNewTabInOppositeMode(info.linkUrl, tab.incognito);
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  let link;
+  let removeTab = false;
+  switch(info.menuItemId) {
+    case "linkItem":
+      link = info.linkUrl;
+      break;
+    case "pageItem":
+      link = info.pageUrl;
+      removeTab = true;
+      break;
+    case "selectionItem":
+      let text = info.selectionText.trim();
+      if (isURL(text)) {
+        link = text;
+      }
+      else {
+        // no need to encode?
+        link = 'https://www.google.com/search?q=' + text;
+      }
+      break;
+  }
+  if (link) {
+    createNewTabInOppositeMode(link, tab.incognito);
+  }
+  if (removeTab) {
+    chrome.tabs.remove(tab.id);
+  }
 });
 
 function createNewTabInOppositeMode(url, incognito) {
-  chrome.windows.getAll({ windowTypes: ["normal"] }, function (windows) {
-    for (var i = 0; i < windows.length; i++) {
+  chrome.windows.getAll({ windowTypes: ["normal"] }, (windows) => {
+    for (let i = 0; i < windows.length; i++) {
       if (windows[i].incognito != incognito) {
         chrome.windows.update(windows[i].id, { focused: true }, function (focused_window) {
           chrome.tabs.create({ windowId: focused_window.id, url: url, active: true });
@@ -25,4 +63,14 @@ function createNewTabInOppositeMode(url, incognito) {
     }
     chrome.windows.create({ url: url, incognito: !incognito });
   });
+}
+
+function isURL(text) {
+  let url;
+  try {
+    url = new URL(text);
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
